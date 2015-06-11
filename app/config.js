ff2f28e78f9d1dc31215f481944e08a8c0cb54df
Hack Reactor Students <accounts@hackreactor.com>
2015-06-11 11:11:39 -0700

    Mongo refactor: signup and login working

--- app/config.js
+++ app/config.js
@@ -1,79 +1,46 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var Promise = require('bluebird');
 
 var db = mongoose.connection;
 
 var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
 
+var UrlSchema = new Schema({
+  url: String,
+  base_url: String,
+  code: String,
+  title: String,
+  visits: Number,
+});
+
+var UserSchema = new Schema({
+  username: String,
+  password: String
+});
+
+UserSchema.methods.comparePassword = function(attemptedPassword, callback) {
+  console.log('Called');
+    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+      callback(isMatch);
+    });
+  },
+
+UserSchema.methods.hashPassword = function(){
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.get('password'), null, null).bind(this)
+    .then(function(hash) {
+      this.set('password', hash);
+    });
+}
+
+exports.UrlSchema = UrlSchema;
+exports.UserSchema = UserSchema;
 db.once('open', function() {
 
-  var UrlSchema = new Schema({
-    url: String,
-    base_url: String,
-    code: String,
-    title: String,
-    visits: Number,
-  });
-
-  var UserSchema = new Schema({
-    username: String,
-    password: String
-  });
-///////////////////////////////////
-///TODO
-//////////////////////////////////
-  var LinkModel = exports.LinkModel = mongoose.model('Link', UrlSchema);
-  exports.UserModel = mongoose.model('User', UserSchema);
-  var newModel = exports.LinkModel({
-      url: "http://www.starbucks.com"
-  });
-  newModel.save();
 });
 
 
 
 mongoose.connect('mongodb://localhost/shawtlyDB');
-// module.exports = db;
-// var Bookshelf = require('bookshelf');
-// var path = require('path');
-
-// var db = Bookshelf.initialize({
-//   client: 'sqlite3',
-//   connection: {
-//     host: '127.0.0.1',
-//     user: 'your_database_user',
-//     password: 'password',
-//     database: 'shortlydb',
-//     charset: 'utf8',
-//     filename: path.join(__dirname, '../db/shortly.sqlite')
-//   }
-// });
-
-// db.knex.schema.hasTable('urls').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('urls', function (link) {
-//       link.increments('id').primary();
-//       link.string('url', 255);
-//       link.string('base_url', 255);
-//       link.string('code', 100);
-//       link.string('title', 255);
-//       link.integer('visits');
-//       link.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
-
-// db.knex.schema.hasTable('users').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('users', function (user) {
-//       user.increments('id').primary();
-//       user.string('username', 100).unique();
-//       user.string('password', 100);
-//       user.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
