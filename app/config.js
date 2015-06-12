3eed7b4629820c60213a1cafa24278863dfc14ae
Hack Reactor Students <accounts@hackreactor.com>
2015-06-11 19:32:22 -0700

    Grunt pushing to production server.

--- app/config.js
+++ app/config.js
@@ -23,6 +23,15 @@ var UserSchema = new Schema({
   password: String
 });
 
+UserSchema.pre('save', function(next){
+  var cipher = bluebird.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.password = hash;
+      next();
+    });
+});
+
 UserSchema.methods.comparePassword = function(attemptedPassword, callback) {
     bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
       if(err) {
@@ -33,13 +42,13 @@ UserSchema.methods.comparePassword = function(attemptedPassword, callback) {
     });
   },
 
-UserSchema.methods.hashPassword = function(){
-  var cipher = Promise.promisify(bcrypt.hash);
-  return cipher(this.password, null, null).bind(this)
-    .then(function(hash) {
-      this.password = hash;
-    });
-}
+// UserSchema.methods.hashPassword = function(){
+//   var cipher = Promise.promisify(bcrypt.hash);
+//   return cipher(this.password, null, null).bind(this)
+//     .then(function(hash) {
+//       this.password = hash;
+//     });
+// }
 
 UrlSchema.methods.createShortcode = function(){
       var shasum = crypto.createHash('sha1');
